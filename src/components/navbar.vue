3421ba5e46b6fcb73e3066951a0e876033d8b917
simbaaf <simocharafi8@gmail.com>
2019-04-05 22:55:02 +0100

    update_fetch

---
+++ src/components/navbar.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="navbar is-primary has-shadow">
+        <div class="container">
+            
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: 'navbar',
+    props: {
+        isActive : 'false'
+    },
+
+}
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="app">
+        
+    </div>
+</template>
+<script>
+import axios from "axios"
+import vue from "vue"
+import Router from "vue-Router"
+import navbar from '../components/navbar.vue';
+
+ export default { 
+     components: { navbar },
+     data : () => {
+         return {
+             GithubArr: [],
+
+         }
+     },
+   
+    }  
+     
+     
+ 
+</script>
+ <style scoped>
+ 
+ </style>
+ 
